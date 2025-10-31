c993ff0b058894e9a2d6a4049751daefd60508db
Abdulrahman Alassaf. <163548732+abdulrahman-3sf@users.noreply.github.com>
2025-10-31 22:36:35 +0300

    print out the content of the post & change title using if statement

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Abdulrahman',
+        'title': 'Blog Post 1',
+        'content': 'This is my first post!',
+        'date_posted': 'October 29, 2025'
+    },
+    {
+        'author': 'Khaled',
+        'title': 'Blog Post 2',
+        'content': 'I\'m trying this blog webiste!',
+        'date_posted': 'October 31, 2025'
+    },
+]
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts,
+        'title': 'HOMEEEE'
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About Blog'})
